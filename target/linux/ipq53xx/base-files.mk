82aaf5fb608ab16f1d87d181d7088b46e301fa68
RomikB <romikb@mail.ru>
2026-08-25 14:56:55 +0000

    ipq53xx/rd15: enable persistent overlay filesystem
    Signed-off-by: RomikB <romikb@mail.ru>

--- target/linux/ipq53xx/base-files.mk
+++ target/linux/ipq53xx/base-files.mk
@@ -1,4 +1,4 @@
 define Package/base-files/install-target
-	echo 'pi_preinit_ramfs_dir="/etc /lib/wifi /mnt /vendor /ini /cfg /license /lib/firmware/qcn6432"' >> $(1)/lib/preinit/00_preinit.conf
+	echo 'pi_preinit_ramfs_dir="/lib/wifi /mnt /vendor /ini /cfg /license /lib/firmware/qcn6432"' >> $(1)/lib/preinit/00_preinit.conf
 	echo 'pi_overlay_partitions="cfg:/data:"' >> $(1)/lib/preinit/00_preinit.conf
 endef
--- target/linux/ipq53xx/image/rd15.mk
+++ target/linux/ipq53xx/image/rd15.mk
@@ -16,7 +16,6 @@ define Device/xiaomi-rd15-prebuild
 	VID_HDR_OFFSET := 2048
 	UBINIZE_PARTS := kernel=:$(TOPDIR)/target/linux/ipq53xx/rd15/kernel
 	ROOTFS_NAME := ubi_rootfs
-	NO_ROOTFS_DATA := 1
 	IMAGES := factory.ubi
 	IMAGE/factory.ubi := append-ubi
 endef
